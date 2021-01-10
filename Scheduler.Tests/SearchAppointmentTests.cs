a5acbe9604c19fad4a15960840e5fb4dc5a786ac
cecily.dantam@gmail.com <cecily.dantam@gmail.com>
2021-01-10 01:59:35 +0000

    add unit tests

---
+++ Scheduler.Tests/SearchAppointmentTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scheduler.Models;
+using Scheduler.Services;
+using System.Collections.Generic;
+
+namespace Scheduler.Tests
+{
+    [TestClass]
+    public class SearchAppointmentTests
+    {
+        [TestMethod]
+        public void SearchAppointmentsReturnsFullSetWhenSearchTextNull()
+        {
+            var appointments = SetUpAppointments();
+            var results = SearchService.SearchAppointments(appointments, null);
+
+            Assert.AreEqual(appointments.Count, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchAppointmentsReturnsFullSetWhenSearchTextEmptyString()
+        {
+            var appointments = SetUpAppointments();
+            var results = SearchService.SearchAppointments(appointments, "");
+
+            Assert.AreEqual(appointments.Count, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchAppointmentsReturnsFullSetWhenSearchTextWhitespace()
+        {
+            var appointments = SetUpAppointments();
+            var results = SearchService.SearchAppointments(appointments, "       ");
+
+            Assert.AreEqual(appointments.Count, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchAppointmentsReturnsEmptySetWhenNoMatch()
+        {
+            var appointments = SetUpAppointments();
+            var results = SearchService.SearchAppointments(appointments, "description");
+
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchAppointmentsReturnsResultsWhenMatch()
+        {
+            var appointments = SetUpAppointments();
+            var results = SearchService.SearchAppointments(appointments, "Title1");
+
+            Assert.AreEqual(2, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchAppointmentsIsCaseInsensitive()
+        {
+            var appointments = SetUpAppointments();
+            var results = SearchService.SearchAppointments(appointments, "title1");
+
+            Assert.AreEqual(2, results.Count);
+        }
+
+        private List<Appointment> SetUpAppointments()
+        {
+            var appointments = new List<Appointment>();
+            var appointment1 = new Appointment
+            {
+                Id = 1,
+                CustomerId = 1,
+                UserId = 1,
+                Title = "Title1",
+                Description = "Description1",
+                Location = "Location1",
+                Contact = "Contact1",
+                Url = "Url1",
+                Type = "Type1"
+            };
+            var appointment2 = new Appointment
+            {
+                Id = 2,
+                CustomerId = 1,
+                UserId = 1,
+                Title = "Title2",
+                Description = "Description1",
+                Location = "Location1",
+                Contact = "Contact1",
+                Url = "Url1",
+                Type = "Type1"
+            };
+            var appointment3 = new Appointment
+            {
+                Id = 3,
+                CustomerId = 1,
+                UserId = 1,
+                Title = "title1",
+                Description = "Description1",
+                Location = "Location1",
+                Contact = "Contact1",
+                Url = "Url1",
+                Type = "Type1"
+            };
+
+            appointments.Add(appointment1);
+            appointments.Add(appointment2);
+            appointments.Add(appointment3);
+
+            return appointments;
+        }
+    }
+}
---
+++ Scheduler.Tests/SearchCustomersTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scheduler.Models;
+using Scheduler.Services;
+using System.Collections.Generic;
+
+namespace Scheduler.Tests
+{
+    [TestClass]
+    public class SearchCustomersTests
+    {
+        [TestMethod]
+        public void SearchCustomersReturnsFullSetWhenSearchTextNull()
+        {
+            var customers = SetUpCustomers();
+            var results = SearchService.SearchCustomers(customers, null);
+
+            Assert.AreEqual(customers.Count, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchCustomersReturnsFullSetWhenSearchTextEmptyString()
+        {
+            var customers = SetUpCustomers();
+            var results = SearchService.SearchCustomers(customers, "");
+
+            Assert.AreEqual(customers.Count, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchCustomersReturnsFullSetWhenSearchTextWhitespace()
+        {
+            var customers = SetUpCustomers();
+            var results = SearchService.SearchCustomers(customers, "       ");
+
+            Assert.AreEqual(customers.Count, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchCustomersReturnsEmptySetWhenNoMatch()
+        {
+            var customers = SetUpCustomers();
+            var results = SearchService.SearchCustomers(customers, "description");
+
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchCustomersIsCaseInsensitive()
+        {
+            var customers = SetUpCustomers();
+            var results = SearchService.SearchCustomers(customers, "name1");
+
+            Assert.AreEqual(2, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchCustomersReturnsResultsWhenMatch()
+        {
+            var customers = SetUpCustomers();
+            var results = SearchService.SearchCustomers(customers, "Name1");
+
+            Assert.AreEqual(2, results.Count);
+        }
+
+        private List<Customer> SetUpCustomers()
+        {
+            var customers = new List<Customer>();
+            var customer1 = new Customer
+            {
+                Id = 1,
+                CustomerName = "Name1"
+            };
+            var customer2 = new Customer
+            {
+                Id = 2,
+                CustomerName = "name1"
+            };
+            var customer3 = new Customer
+            {
+                Id = 3,
+                CustomerName = "Name2"
+            };
+
+            customers.Add(customer1);
+            customers.Add(customer2);
+            customers.Add(customer3);
+
+            return customers;
+        }
+    }
+}
